ad931a91f4a95844e1267c9940347bc0ee95392d
wilsmithyef <wilsmithyef@gmail.com>
2023-09-12 13:39:51 -0500

    Finished ult v hw 06

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,12 @@
-const listOfItems = document.querySelectorAll("li.item");
-console.log(listOfItems.length);
+const categoriesList = document.querySelectorAll("#categories .item");
+
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const elementsList = category.querySelectorAll("ul li");
+  const numberOfElements = elementsList.length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${numberOfElements}\n`);
+});
